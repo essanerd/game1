307c9fd95317ef2d7a72831b19e8e4e249ebd588
yahyacurry <reduxwolverine@gmail.com>
2024-02-14 18:38:08 -0500

    first commit

---
+++ src/KeyHandler.java
@@ -0,0 +1,26 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        int code  = e.getKeyCode();
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
