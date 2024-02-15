c83bdf1822205eb7efb0d9bd39fad153408dc356
essasnerd <essanerd@gmail.com>
2024-02-15 16:49:39 -0500

    updates

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -14,13 +14,16 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tilesize * maxscreencol;// 768 pixels wide
     final int screenHeight = tilesize * maxscreenrow; // 576 pixels long
 
+    KeyHandler keyH = new KeyHandler();// declaring a new key handler
+
     Thread gameThread;// keeps my programs runing untill i stop it
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));// telling a new dimension
         this.setBackground(Color.black);// setting background color
         this.setDoubleBuffered(true);
-
+        this.addKeyListener(keyH);// so it can recognize the key input
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -29,7 +32,12 @@ public class GamePanel extends JPanel implements Runnable {
         gameThread.start();// telling the thread to start
     }
 
-    @Override
+    // set player`s default posititon
+    int PlayerX = 100;
+    int PlayerY = 100;
+    int Playerspeed = 4;
+
+
     public void run() {//gamethread caller
 
         while (gameThread != null) {//this is telling java that as long as this thread exist keep running what is in the  curly brackets
@@ -45,13 +53,18 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void update() {
+        if ( keyH.upPressed == true) {
+            PlayerY-=Playerspeed;
+            PlayerY = PlayerY - Playerspeed;
+
+        }
     }
 
     public void paintComponent(Graphics g) {// to tell java hey man I want to draw the updated picture
         super.paintComponent(g);// super  is the parent class of this class // cause when ever I type paint component I need this
         Graphics2D g2 = (Graphics2D) g;// has little more functitions than graphics
         g2.setColor(Color.white);// setting color
-        g2.fillRect(100, 100, tilesize, tilesize);// draws a rectangle on the screen
+        g2.fillRect(PlayerX, PlayerY, tilesize, tilesize);// draws a rectangle on the screen
         g2.dispose();// good practice
 
 
--- src/KeyHandler.java
+++ src/KeyHandler.java
@@ -2,8 +2,10 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener {
+    public boolean upPressed, downPressed, leftPressed, rightPressed;
 
-// test comment
+
+    // test comment
 
     @Override
     public void keyTyped(KeyEvent e) {
@@ -11,16 +13,55 @@ public class KeyHandler implements KeyListener {
     }
 
 
-
     @Override
     public void keyPressed(KeyEvent e) {
 
-        int code  = e.getKeyCode();
+        int code = e.getKeyCode();// basicallly returns a number of the key  that was pressed
+
+        if (code == KeyEvent.VK_W) {// if code = w
+            upPressed = true; // w pressed is true
+
+        }
+
+        if (code == KeyEvent.VK_S) {// if code = s
+            downPressed = true; // s pressed is true
+
+        }
+
+        if (code == KeyEvent.VK_A) {// if code = a
+            leftPressed = true; // a pressed is true
+        }
+
+        if (code == KeyEvent.VK_D) {// if code = d
+            rightPressed = true; // d pressed is true
+
+        }
 
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        if (code == KeyEvent.VK_W) {// if code = w
+            upPressed = false; // w pressed is False
+
+        }
+
+        if (code == KeyEvent.VK_S) {// if code = s
+            downPressed = false; // s pressed is false
+
+        }
+
+        if (code == KeyEvent.VK_A) {// if code = a
+            leftPressed = false;  // a pressed is false
+        }
+
+        if (code == KeyEvent.VK_D) {// if code = d
+            rightPressed = false; // d pressed is  false
+
+        }
+
 
     }
 }
